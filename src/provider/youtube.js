cc4c6226bc40047150dbe83b0c8e20ae4ae7d920
Yi-Jyun Pan <pan93412@gmail.com>
2021-06-05 15:14:45 +0800

    feat(provider/youtube): use youtube-dl to get the video

--- src/provider/youtube.js
+++ src/provider/youtube.js
@@ -1,78 +1,55 @@
 const cache = require('../cache')
-const request = require('../request')
-const parse = query => (query || '').split('&').reduce((result, item) => (item = item.split('=').map(decodeURIComponent), Object.assign({}, result, {[item[0]]: item[1]})), {})
+const execa = require('execa');
 
-// const proxy = require('url').parse('http://127.0.0.1:1080')
-const proxy = undefined
-const key = process.env.YOUTUBE_KEY || null // YouTube Data API v3
+/**
+ * The arguments to pass to youtube-dl
+ * 
+ * ```plain
+ * youtube-dl -f bestaudio --dump-json <query>
+ *		-f bestaudio 	choose the best quality of the audio
+ *		--dump-json		dump the information as JSON without downloading it
+ * ```
+ * 
+ * @param {string} query 
+ */
+const dlArguments = (query) => ["-f", "140", "--dump-json", query];
+/** @param {string} id */
+const byId = (id) => `https://www.youtube.com/watch?v=${id}`;
+/** @param {string} keyword */
+const byKeyword = (keyword) => `ytsearch1:${keyword}`;
 
-const signature = (id = '-tKVN2mAKRI') => {
-	const url =
-		`https://www.youtube.com/watch?v=${id}`
+/**
+ * Checking if youtube-dl is available,
+ * then execute the command and extract the ID and URL.
+ * 
+ * @param {string[]} args
+ * @returns {Promise<{id: string, url: string}>}
+ */
+async function getUrl(args) {
+	const { stdout } = await execa("youtube-dl", args);
+	const response = JSON.parse(stdout);
+	if (
+		typeof response === "object" &&
+		typeof response.id === "string" &&
+		typeof response.url === "string"
+	) {
+		console.log(response.url);
+		return response;
+	}
 
-	return request('GET', url, {}, null, proxy)
-	.then(response => response.body())
-	.then(body => {
-		let assets = /"WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_VERTICAL_LANDING_PAGE_PROMO":{[^}]+}/.exec(body)[0]
-		assets = JSON.parse(`{${assets}}}`).WEB_PLAYER_CONTEXT_CONFIG_ID_KEVLAR_VERTICAL_LANDING_PAGE_PROMO
-		return request('GET', 'https://youtube.com' + assets.jsUrl, {}, null, proxy).then(response => response.body())
-	})
-	.then(body => {
-		const [_, funcArg, funcBody] = /function\((\w+)\)\s*{([^}]+split\(""\)[^}]+join\(""\))};/.exec(body)
-		const helperName = /;(.+?)\..+?\(/.exec(funcBody)[1]
-		const helperContent = new RegExp(`var ${helperName}={[\\s\\S]+?};`).exec(body)[0]
-		return new Function([funcArg], helperContent + '\n' + funcBody)
-	})
+	throw new Error("the response from youtube-dl is invalid.");
 }
 
-const apiSearch = info => {
-	const url =
-		`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(info.keyword)}&type=video&key=${key}`
-
-	return request('GET', url, {accept: 'application/json'}, null, proxy)
-	.then(response => response.json())
-	.then(jsonBody => {
-		const matched = jsonBody.items[0]
-		if (matched)
-			return matched.id.videoId
-		else
-			return Promise.reject()
-	})
+const search = async (info) => {
+	const { id } = await getUrl(dlArguments(byKeyword(info.keyword)));
+	return id;
 }
 
-const search = info => {
-	const url =
-		`https://www.youtube.com/results?search_query=${encodeURIComponent(info.keyword)}`
-
-	return request('GET', url, {}, null, proxy)
-	.then(response => response.body())
-	.then(body => {
-		const initialData = JSON.parse(body.match(/ytInitialData\s*=\s*([^;]+);/)[1])
-		const matched = initialData.contents.twoColumnSearchResultsRenderer.primaryContents.sectionListRenderer.contents[0].itemSectionRenderer.contents[0]
-		if (matched)
-			return matched.videoRenderer.videoId
-		else
-			return Promise.reject()
-	})
+const track = async id => {
+	const { url } = await getUrl(dlArguments(byId(id)));
+	return url;
 }
 
-const track = id => {
-	const url =
-		`https://www.youtube.com/get_video_info?video_id=${id}&el=detailpage`
-
-	return request('GET', url, {}, null, proxy)
-	.then(response => response.body())
-	.then(body => JSON.parse(parse(body).player_response).streamingData)
-	.then(streamingData => {
-		const stream = streamingData.formats.concat(streamingData.adaptiveFormats)
-		.find(format => format.itag === 140)
-		// .filter(format => [249, 250, 140, 251].includes(format.itag)) // NetaseMusic PC client do not support webm format
-		// .sort((a, b) => b.bitrate - a.bitrate)[0]
-		const target = parse(stream.signatureCipher)
-		return stream.url || (target.sp.includes('sig') ? cache(signature, undefined, 24 * 60 * 60 * 1000).then(sign => target.url + '&sig=' + sign(target.s)) : target.url)
-	})
-}
-
-const check = info => cache(key ? apiSearch : search, info).then(track)
+const check = info => cache(search, info).then(track)
 
 module.exports = {check, track}
